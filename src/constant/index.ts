2d766a1bd6b24be2e41f4c30a3b0263566513b71
ducmanh1101 <jake@beraji.io>
2025-12-16 16:57:26 +0700

    Update index.ts

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -10,5 +10,5 @@ export const MAX_FUND = 1_000_000
 export const MIN_GOAL = 200
 export const MAX_GOAL = 2_000_000
 
-export const TIME_CLOSED_BETA = '2026-01-06T00:00:00.000Z'
+export const TIME_CLOSED_BETA = '2026-01-06T15:00:00.000Z'
 export const APP_LINK = 'https://app.primevaults.finance/'
